43b167fe59135930c9170cafea549b330157bfeb
Reifier <nwe.apinchuk@us.westfield.com>
2017-12-26 13:42:38 -0800

    Refactoring

--- bit_string_into_decimal.c
+++ bit_string_into_decimal.c
@@ -1,4 +1,4 @@
-/* An unprotected primitive to quickly translate a binary byte into a decimal */
+/* An unsafe primitive to quickly translate a binary byte into a decimal */
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -10,11 +10,9 @@ int main () {
   printf("Please input string of 8 bytes that you would like to decimate:\n ");
   fgets(bytes,9,stdin);
   int i;
-  int position;
   int value = 0;
   int number;
   for(i = 0; i <= 7; i++) {
-    position = i + 1;
     number = bytes[i] - '0';
     value = value + pow(2, 7 - i) * number;
     number = bytes[i] - '0';
